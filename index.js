447999c7566e68e887df5379a574e7cac90f49d1
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-11-06 09:55:58 +0530

    Resolve cors errors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRoutes = require('./routes/route');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/', userRoutes);
 
